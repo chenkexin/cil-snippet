747974a5c83cecbd63582a7a54522c91619e1de0
chenkexin <chenkexin.cq@gmail.com>
2014-12-08 11:07:37 +0800

    add view in test_macro

--- ciltest4.ml
+++ ciltest4.ml
@@ -36,10 +36,66 @@ class visitor( target_vname, target_funcname: string*string) = object(self)
 
   method print_List = ()
 
+  method print_type t =
+    E.log "type: %a\n" d_type t;
+  method print_const c = 
+    E.log "const: %a\n" d_const c;
+  method print_unop u =
+    E.log "unop: ignore\n";
+  method print_binop b = 
+    E.log "binop: ignore\n";
+  method print_string s = 
+    E.log "string: %s\n" s;
+
+  method introspect_exp (e:exp) = 
+    let rec match_e e = 
+      let rec print_lv lv = 
+        (* E.log "in match lv: %a\n" d_lval lv; *)
+        let match_lv_fst  t_lv = 
+          match fst t_lv with
+          | Var(v) -> E.log "var: %s\n" v.vname
+          | Mem(m) -> match_e m
+        in
+        let  match_lv_snd t_lv = 
+          let rec offset_helper o = 
+            match o with
+            |NoOffset -> ();
+            |Field(f, t_o) -> E.log "field: %s\n" f.fname; offset_helper t_o;
+            |Index(e, t_o) -> match_e e; offset_helper t_o;
+          in
+          offset_helper (snd t_lv);
+        in
+        match_lv_fst lv;
+        match_lv_snd lv;
+      in
+      match e with
+      | Const(c) -> self#print_const c;
+      | SizeOf(t) -> self#print_type t;
+      | Lval(lv) -> print_lv lv;
+      | SizeOfE(e) ->match_e e;
+      | AlignOfE(e) -> match_e e;
+      | SizeOfStr(s) -> self#print_string s;
+      | UnOp(u,e,t) -> 
+          self#print_unop(u);  
+          match_e e; 
+          self#print_type t;
+      | BinOp(b,e1,e2,t) ->
+          self#print_binop(b); 
+          match_e e1; 
+          match_e e2;
+          self#print_type t;
+      | CastE(t,e) ->
+          self#print_type t;
+          match_e e;
+      | AddrOf(lv) -> print_lv lv;
+      | StartOf(lv) -> print_lv lv;
+      | AlignOf(t) -> self#print_type t;
+    in
+    match_e e;
+
   method vinst( i: instr) :instr list visitAction = 
     match i with
-    | Set( lv, e, loc) -> E.log "in vinst.Set: right exp %a loc %a\n" d_exp e d_loc
-    loc; E.log "\t lval: %a\n" d_lval lv;DoChildren
+    | Set( lv, e, loc) ->E.log "loc: %a\n" d_loc loc; self#introspect_exp e; DoChildren
     | Call(lv, e1, e2, loc) -> E.log "in vinst.Call: e1 %a loc %a\n" d_exp e1
     d_loc loc; DoChildren
     | _ -> E.log" in vinst._\n"; DoChildren
--- test_macro.c
+++ test_macro.c
@@ -1,5 +1,6 @@
 #include "test_macro.h"
 #include <stdio.h>
+#include "test1.h"
 
 #define bn_check_top(a) \
   do { \
@@ -28,4 +29,22 @@ void BN_free(int *t)
 int* BN_new()
 {
   return malloc(sizeof(int));
+}
+
+int main()
+{
+  struct rsa_st* p_rsa_st = calloc(sizeof(struct rsa_st), 0);
+  struct evp_pkey_st* p_evp_pkey_st = calloc(sizeof(struct evp_pkey_st), 0);
+  CERT_PKEY* p_cert_pkey = calloc(sizeof(CERT_PKEY), 0);
+  CERT* p_cert = calloc(sizeof(CERT), 0);
+
+  SSL* p_ssl = calloc(sizeof(SSL), 0);
+
+  p_evp_pkey_st->pkey.rsa = p_rsa_st;
+  p_cert_pkey->privateKey = p_evp_pkey_st;
+
+  p_cert->pkey = p_cert_pkey;
+
+  p_rsa_st = p_cert->pkey->privateKey->pkey.rsa; 
+
 }
